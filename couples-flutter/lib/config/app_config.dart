bef967050ccfbdea1b6823323c28e4b32c92f04a
Nitish Tao <pixsans26@gmail.com>
2026-06-27 15:50:50 +0530

    config: change Flutter API URL to 127.0.0.1 for USB ADB reverse proxy stability

--- couples-flutter/lib/config/app_config.dart
+++ couples-flutter/lib/config/app_config.dart
@@ -1,4 +1,4 @@
-const String kApiBaseUrl = 'http://192.168.1.5:4000';
+const String kApiBaseUrl = 'http://127.0.0.1:4000';
 const String kApiUrl = '$kApiBaseUrl/api';
 
 // Firebase config is loaded via google-services.json / GoogleService-Info.plist
